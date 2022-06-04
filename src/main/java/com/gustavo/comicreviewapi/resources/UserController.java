77e9a2e1ba8de47921518cc0c90a4e1c04813606
Gustavo Cruz <gu.cruz17@hotmail.com>
2022-06-04 17:17:39 -0300

    Creating the save in UserController and the tests

---
+++ src/main/java/com/gustavo/comicreviewapi/resources/UserController.java
@@ -0,0 +1,34 @@
+package com.gustavo.comicreviewapi.resources;
+
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.gustavo.comicreviewapi.dtos.UserDTO;
+import com.gustavo.comicreviewapi.dtos.UserNewDTO;
+import com.gustavo.comicreviewapi.services.UserService;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@PostMapping
+	public ResponseEntity<UserDTO> save(@RequestBody UserNewDTO userNewDto) {
+		UserDTO userDto = userService.save(userNewDto);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				 .buildAndExpand(userDto.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(userDto);
+	}
+
+}
