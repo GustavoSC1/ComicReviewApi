810cc21b937f624d0e37a84ff8e1922c2822c634
Gustavo Cruz <gu.cruz17@hotmail.com>
2022-06-03 23:13:39 -0300

    Project created

---
+++ src/main/java/com/gustavo/comicreviewapi/ComicReviewApiApplication.java
@@ -0,0 +1,13 @@
+package com.gustavo.comicreviewapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicReviewApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComicReviewApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gustavo/comicreviewapi/ComicReviewApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavo.comicreviewapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComicReviewApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
