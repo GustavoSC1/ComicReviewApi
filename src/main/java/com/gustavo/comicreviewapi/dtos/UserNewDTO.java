169a93ad1375462d2231bb38c1aa9d3c47eaeff8
Gustavo Cruz <gu.cruz17@hotmail.com>
2022-06-04 21:32:21 -0300

    Creating validation test to insert a new user

--- src/main/java/com/gustavo/comicreviewapi/dtos/UserNewDTO.java
+++ src/main/java/com/gustavo/comicreviewapi/dtos/UserNewDTO.java
@@ -3,12 +3,29 @@ package com.gustavo.comicreviewapi.dtos;
 import java.io.Serializable;
 import java.time.LocalDate;
 
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+
 public class UserNewDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@NotEmpty(message="Mandatory completion")
+	@Length(min=8, max=120, message="The length must be between 8 and 120 characters")
 	private String name;
+	
+	@NotNull(message="Mandatory completion")
+	@Past(message="Invalid date")
 	private LocalDate birthDate;
+	
+	@NotEmpty(message="Mandatory completion")
 	private String phone;
+	
+	@NotEmpty(message="Mandatory completion")
+	@Email(message="Invalid email")
 	private String email;
 		
 	public UserNewDTO() {
