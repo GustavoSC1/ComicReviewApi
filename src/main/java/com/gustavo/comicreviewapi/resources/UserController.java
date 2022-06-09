e2a92ceb14fddf974efa91ad367a4fc665e9f6fb
Gustavo Cruz <gu.cruz17@hotmail.com>
2022-06-09 00:16:56 -0300

    Creating the update in UserController and the tests

--- src/main/java/com/gustavo/comicreviewapi/resources/UserController.java
+++ src/main/java/com/gustavo/comicreviewapi/resources/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,6 +41,13 @@ public class UserController {
 		UserDTO userDto = userService.find(id);
 		
 		return ResponseEntity.ok().body(userDto);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<UserDTO> update(@PathVariable Long id, @RequestBody UserDTO userDto) {
+		UserDTO user = userService.update(id, userDto);
+		
+		return ResponseEntity.ok().body(user);
 	}
 
 }
