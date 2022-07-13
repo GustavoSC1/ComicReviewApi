8ce644a24ff4ec38adf1df80b3cb3fba52d5d58c
Gustavo Cruz <gu.cruz17@hotmail.com>
2022-07-13 00:55:16 -0300

    Creating the save in ReviewController and the tests

---
+++ src/main/java/com/gustavo/comicreviewapi/resources/ReviewController.java
@@ -0,0 +1,34 @@
+package com.gustavo.comicreviewapi.resources;
+
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.gustavo.comicreviewapi.dtos.ReviewDTO;
+import com.gustavo.comicreviewapi.dtos.ReviewNewDTO;
+import com.gustavo.comicreviewapi.services.ReviewService;
+
+@RestController
+@RequestMapping("/reviews")
+public class ReviewController {
+	
+	@Autowired
+	private ReviewService reviewService;
+	
+	@PostMapping
+	public ResponseEntity<ReviewDTO> save(@RequestBody ReviewNewDTO reviewNewDto) {
+		ReviewDTO reviewDto = reviewService.save(reviewNewDto);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(reviewDto.getId()).toUri();
+	
+		return ResponseEntity.created(uri).body(reviewDto);
+	}
+
+}
