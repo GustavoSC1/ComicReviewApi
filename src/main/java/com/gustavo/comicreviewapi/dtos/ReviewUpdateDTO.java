086efbb5b4b335d11bd508c2b526fb9e58b3c6a1
Gustavo Cruz <gu.cruz17@hotmail.com>
2022-07-17 20:27:41 -0300

    Creating validation test to update a review

--- src/main/java/com/gustavo/comicreviewapi/dtos/ReviewUpdateDTO.java
+++ src/main/java/com/gustavo/comicreviewapi/dtos/ReviewUpdateDTO.java
@@ -2,10 +2,19 @@ package com.gustavo.comicreviewapi.dtos;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 public class ReviewUpdateDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@NotEmpty(message="The title field is required")
+	@Length(min=8, max=120, message="The length must be between 8 and 120 characters")
 	private String title;
+	
+	@NotEmpty(message="The content field is required")
+	@Length(min=40, message="Content field cannot be less than 40 characters")
 	private String content;
 	
 	public ReviewUpdateDTO() {
