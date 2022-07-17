b1fd0e8194e7475b3bfef579ef20cbf5074064e4
Gustavo Cruz <gu.cruz17@hotmail.com>
2022-07-17 19:02:44 -0300

    Creating the update in ReviewController and the tests

--- src/main/java/com/gustavo/comicreviewapi/resources/ReviewController.java
+++ src/main/java/com/gustavo/comicreviewapi/resources/ReviewController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.gustavo.comicreviewapi.dtos.ReviewDTO;
 import com.gustavo.comicreviewapi.dtos.ReviewNewDTO;
+import com.gustavo.comicreviewapi.dtos.ReviewUpdateDTO;
 import com.gustavo.comicreviewapi.services.ReviewService;
 
 @RestController
@@ -40,6 +42,13 @@ public class ReviewController {
 		ReviewDTO reviewDto = reviewService.find(id);
 		
 		return ResponseEntity.ok().body(reviewDto);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<ReviewDTO> update(@PathVariable Long id, @RequestBody ReviewUpdateDTO reviewDto) {
+		ReviewDTO review = reviewService.update(id, reviewDto);
+		
+		return ResponseEntity.ok().body(review);
 	}
 
 }
