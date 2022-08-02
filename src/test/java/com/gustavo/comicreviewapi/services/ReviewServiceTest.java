dd75f858522642f64a92d33bb4107bda6903db63
Gustavo Cruz <gu.cruz17@hotmail.com>
2022-08-01 21:26:33 -0300

    Adding ReviewNewDtoBuilder to saveReviewTest in ReviewServiceTest

--- src/test/java/com/gustavo/comicreviewapi/services/ReviewServiceTest.java
+++ src/test/java/com/gustavo/comicreviewapi/services/ReviewServiceTest.java
@@ -16,6 +16,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.gustavo.comicreviewapi.builders.ComicBuilder;
 import com.gustavo.comicreviewapi.builders.ReviewBuilder;
+import com.gustavo.comicreviewapi.builders.ReviewNewDtoBuilder;
 import com.gustavo.comicreviewapi.builders.UserBuilder;
 import com.gustavo.comicreviewapi.dtos.ReviewDTO;
 import com.gustavo.comicreviewapi.dtos.ReviewNewDTO;
@@ -54,7 +55,7 @@ public class ReviewServiceTest {
 		// Scenario
 		Long id = 1l;
 		
-		ReviewNewDTO newReview = createReviewNewDTO();
+		ReviewNewDTO newReview = ReviewNewDtoBuilder.aReviewNewDTO().now();
 		User user = UserBuilder.aUser().withId(id).now();
 		Comic comic = ComicBuilder.aComic().withAuthorsList(new Author(null, "Stefan Petrucha"))
 				.withCharactersList(new Character(null, "Homem Aranha")).withId(id).now();
@@ -182,13 +183,4 @@ public class ReviewServiceTest {
 				+ "família.");
 	}
 	
-	public ReviewNewDTO createReviewNewDTO() {
-		return new ReviewNewDTO("Ótima história", "A HQ mostra o Homem-Aranha em sua essência: "
-				+ "cheio de problemas, tentando fazer o que é certo enquanto luta para manter sua identidade secreta em "
-				+ "segredo, com um turbilhão de coisas acontecendo ao mesmo tempo, na escola, no namoro, no trabalho, em "
-				+ "família. É maravilhoso ver a determinação do herói e impossível não se identificar com ele, não se agoniar "
-				+ "com seus problemas e torcer pela sua vitória. É tudo que se espera de uma boa aventura de super-heróis e "
-				+ "um roteiro perfeito para um filme do Aracnídeo.", 1l, 1l);
-	}		
-
 }
