ab361b1415fdb9e7041f896ae8477c47cf3b6747
Gustavo Cruz <gu.cruz17@hotmail.com>
2022-08-01 22:02:17 -0300

    Creating UserNewDtoBuilder and updating tests

---
+++ src/test/java/com/gustavo/comicreviewapi/builders/UserNewDtoBuilder.java
@@ -0,0 +1,28 @@
+package com.gustavo.comicreviewapi.builders;
+
+import java.time.LocalDate;
+
+import com.gustavo.comicreviewapi.dtos.UserNewDTO;
+
+public class UserNewDtoBuilder {
+	
+	private UserNewDTO userNewDto;
+	
+	private UserNewDtoBuilder() {}
+	
+	public static UserNewDtoBuilder aUserNewDTO() {
+		UserNewDtoBuilder builder = new UserNewDtoBuilder();
+		builder.userNewDto = new UserNewDTO();
+		builder.userNewDto.setName("Gustavo Silva Cruz");
+		builder.userNewDto.setEmail("gu.cruz17@hotmail.com");
+		builder.userNewDto.setBirthDate(LocalDate.of(1996, 10, 17));
+		builder.userNewDto.setPhone("998123456");
+
+		return builder;
+	}
+	
+	public UserNewDTO now() {
+		return userNewDto;
+	}	
+
+}
