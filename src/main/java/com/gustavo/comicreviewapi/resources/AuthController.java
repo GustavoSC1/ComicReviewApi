c5544c1769d53da0771cfe99ee8da1714feae90b
Gustavo Cruz <gu.cruz17@hotmail.com>
2022-09-07 20:56:53 -0300

    Creating the Refresh Token

---
+++ src/main/java/com/gustavo/comicreviewapi/resources/AuthController.java
@@ -0,0 +1,32 @@
+package com.gustavo.comicreviewapi.resources;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gustavo.comicreviewapi.security.JWTUtil;
+import com.gustavo.comicreviewapi.security.UserSS;
+import com.gustavo.comicreviewapi.services.UserService;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+	
+	@Autowired
+	private JWTUtil jwtUtil;
+	
+	// Atualiza o token - O usuário precisa estra logado para acessar
+	@PostMapping("/refresh_token")
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+		UserSS user = UserService.authenticated();
+		String token = jwtUtil.generateToken(user.getUsername());
+		response.addHeader("Authorization", "Bearer " + token);
+		response.addHeader("access-control-expose-headers", "Authorization");
+		return ResponseEntity.noContent().build();
+	}
+
+}
